6281166cdbf7f6c9c36beb3c0a78a7917a6a4715
Mars Zuo <marszuo@foxmail.com>
2022-12-09 20:31:41 +0800

    feat: Package the admin-ui page

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 ## Monica 服务治理平台
+
+1.编译 admin-ui
+
+```shell
+yarn build
+mv dist/ ../dist 
+```
+
+2.二进制包部署
+
+```shell
+go build -o monica
+chmod +x restart.sh
+./restart.sh
+```
+
+3.Docker 部署
+
+```shell
+# 编译镜像
+docker build -t ccr.ccs.tencentyun.com/tcb-xxx-xupz/prod-xxx-online:[tag] .
+
+# 推送镜像
+docker push ccr.ccs.tencentyun.com/tcb-xxx-xupz/prod-xxxx-online:[tag]
+```
--- web/router.go
+++ web/router.go
@@ -4,6 +4,9 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
 	"monica/app/interfaces"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 type Router struct {
@@ -29,4 +32,32 @@ func (r *Router) registerHandler(s *interfaces.AdminHttpServiceImpl) {
 	r.Handle("/api/v1/service_instances", r.ch.ThenFunc(s.DoServiceInstanceList)).Methods("GET")
 	r.Handle("/api/v1/service_instance", r.ch.ThenFunc(s.DoSaveServiceInstance)).Methods("POST")
 	r.Handle("/api/v1/service_instance", r.ch.ThenFunc(s.DoDeleteServiceInstance)).Methods("DELETE")
+
+	// 静态资源
+	r.PathPrefix("/").Handler(vueRouter("/", http.FileServer(http.Dir("./dist"))))
+}
+
+func vueRouter(prefix string, h http.Handler) http.Handler {
+	if prefix == "" {
+		return h
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		p := strings.TrimPrefix(r.URL.Path, prefix)
+		// 适配 Vue 路由
+		if !strings.HasPrefix(p, "api/") {
+			p = ""
+		}
+		rp := strings.TrimPrefix(r.URL.RawPath, prefix)
+		if len(p) < len(r.URL.Path) && (r.URL.RawPath == "" || len(rp) < len(r.URL.RawPath)) {
+			r2 := new(http.Request)
+			*r2 = *r
+			r2.URL = new(url.URL)
+			*r2.URL = *r.URL
+			r2.URL.Path = p
+			r2.URL.RawPath = rp
+			h.ServeHTTP(w, r2)
+		} else {
+			http.NotFound(w, r)
+		}
+	})
 }
